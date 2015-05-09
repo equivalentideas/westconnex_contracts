4ad66313cb924c03d2c598dc3de47dcb1763a9aa
Luke Bacon <luke@equivalentideas.com>
2015-05-10 09:39:47 +1000

    Remove template comments

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,3 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
-
 def row_value(row)
   row.at(:td).text
 end
@@ -118,15 +115,3 @@ while start_row >= 0
     start_row = -1
   end
 end
-
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
